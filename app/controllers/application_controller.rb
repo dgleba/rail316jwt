30ab7b3f6dbde7fbf299114d420487e8f801b9a1
David Gleba <dgleba@gmail.com>
2016-12-26 21:19:12 -0500

    add cancan 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ before_filter :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    # redirect_to :back, alert: exception.message
+    redirect_to main_app.root_path, :alert => exception.message
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  resources :roles
   devise_for :users
  root "home#index"
 
