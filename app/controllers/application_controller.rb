98343f40b40bee361f36af6297686f7aeb5c334e
David Gleba <dgleba@gmail.com>
2016-12-27 11:26:16 -0500

    fix "Can't verify CSRF token authenticity"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
-before_filter :authenticate_user!
+
+  before_filter :authenticate_user!
+
+  # Be sure to enable JSON.
+  respond_to :html, :json
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
+ 
   rescue_from CanCan::AccessDenied do |exception|
     # redirect_to :back, alert: exception.message
     redirect_to main_app.root_path, :alert => exception.message
